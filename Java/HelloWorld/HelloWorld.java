0c52cf61ed06c83d65a9f590db07414fdf499e4e
seon <joao.carvalho315@etec.sp.gov.br>
2024-05-27 10:42:48 -0300

    Adding Java

--- Java/HelloWorld/HelloWorld.java
+++ Java/HelloWorld/HelloWorld.java
@@ -1,5 +1,5 @@
 package com.eteczonaleste.seonprojeto;
-public class Seonprojeto {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
